c950ec91552fe9041f21fdea5880a2ebbb2c8d1b
Delucse <46593742+Delucse@users.noreply.github.com>
2020-06-10 12:41:58 +0200

    approach

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
+import Principles from './Principles';
+
+import Grid from '@material-ui/core/Grid';
+
+
 class Home extends Component {
   render() {
     return (
-      <h1>Startseite</h1>
+      <div style={{margin: '0px 24px'}}>
+        <h1 style={{textAlign: 'center'}}>Unser Ansatz</h1>
+        <div style={{backgroundColor: 'lightgrey', padding: '15px', margin: '0 -24px', textAlign: 'center'}}>
+          <div style={{margin: '0px 10%'}}>
+            <p>Unser Feedbacksystem motiviert die Teilnehmenden mit Freude zu lernen und verhilft ihnen zu einer realistischen Selbsteinschätzung ihres eigenen Fortschritts. Gleichzeitig bietet es der Lernbegleitung Orientierung über den Leistungsstand und bildet damit eine wichtige Grundlage für die weitere didaktisch-pädagogische Planung des Lernprozesses.</p>
+          </div>
+        </div>
+        <h3 style={{textAlign: 'center', marginBottom: '30px'}}>Dabei baut unser Ansatz auf 6 Prinzipien auf:</h3>
+        <Grid container spacing={4}>
+          <Principles title='#offen' content=' Andere Einrichtungen können das System entsprechend ihrer Bildungsangebote ausbauen und weiterentwickeln.'/>
+          <Principles title='#transparent & flexibel' content='Der Lernende hat Einblick über sein Portfolio und kann selbstbestimmt entscheiden, wem er welche Informationen zur Verfügung stellt.'/>
+          <Principles title='#standardisiert' content='Durch den Zusammenschluss verschiedener Bildungsakteure wird ein Standardisierungswerkzeug geschaffen, das Orientierung bietet.'/>
+          <Principles title='#motivierend' content='“Portfolio & Badges” sind eine Form der Leistungsdarstellung; nicht der Bewertung. Sie fördern die Wertschätzung für die erworbenen Kompetenzen und steigern die Selbstverantwortung des Lernenden.'/>
+          <Principles title='#digital & dezentral' content='Wir setzen auf etablierte technische Standards auf, mit denen Kompetenznachweise digital erstellt und dezentral verwaltet werden können.'/>
+          <Principles title='#individuell' content='“Portfolio & Badges” können die individuellen Lernwege dokumentieren und fördern somit das selbstbestimmte, lebenslange Lernen.'/>
+        </Grid>
+      </div>
     );
   };
 }
---
+++ src/components/home/Principles.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Grid from '@material-ui/core/Grid';
+
+class Principles extends Component {
+  render() {
+    return (
+      <Grid item xs={12} sm={6} md={4} style={{textAlign: 'center'}}>
+        <img src={this.props.icon} width='50' height='50'/>
+        <h4 style={{margin:'5px'}}>{this.props.title}</h4>
+        {this.props.content}
+      </Grid>
+    );
+  };
+}
+
+export default Principles;
