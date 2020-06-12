7fe33a7b591fc4d6602e21f8affcddda734781a2
felixerdy <felix.erdmann@uni-muenster.de>
2020-06-12 16:56:40 +0200

    add youtube video

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -31,6 +31,12 @@ class Home extends Component {
               <h3>Ein lernerzentrierter Ansatz zur Dokumentation & Feedback des Lernprozesses</h3>
             </div>
           </div>
+        <Spacer></Spacer>
+        <div style={{position: 'relative', paddingBottom: '56.25%', height: 0}}>
+          <iframe style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}} src="https://www.youtube.com/embed/7VL3kqI8CxY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <Spacer></Spacer>
+
         <div style={{backgroundColor: '#ffebce', padding: '5rem', textAlign: 'center', position: 'relative'}}>
           <h1 style={{textAlign: 'center'}}>Unsere Idee</h1>
           <div style={{margin: '0px 10%'}}>
